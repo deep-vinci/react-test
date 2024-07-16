b24ea3a74b57a516eac359944d6156dd6d3726d7
Deepak <deepakworkpc@gmail.com>
2024-07-17 00:14:15 +0530

    refactor: use the components List and ListItem

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+function ListItem(props) {
+    return <li>{props.animalVar}</li>;
+}
+
+function List(props) {
+    return (
+        <ul>
+            {props.animalList.map(animal => {
+                return <ListItem key={animal} animalVar={animal} />;
+            })}
+        </ul>
+    )
+}
+
 function App() {
     const animals = ["Lions", "Cow", "Snake", "Lizard"];
 
     return (
         <div>
             <h1>Animals: </h1>
-            <ul>{animals.map((animal) => {
-                return <li key={animal}>{animal}</li>;
-            })}</ul>
+            <List animalList={animals} />
         </div>
     );
 }
