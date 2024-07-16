8c5cc5eb62b654e58748b5261854a7878a17a825
Deepak <deepakworkpc@gmail.com>
2024-07-16 23:49:49 +0530

    feat: add animals list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const animals = ["Lions", "Cow", "Snake", "Lizard"];
 
-  return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
-  )
+    return (
+        <div>
+            <h1>Animals: </h1>
+            <ul>{animals.map((animal) => {
+                return <li key={animal}>{animal}</li>;
+            })}</ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
